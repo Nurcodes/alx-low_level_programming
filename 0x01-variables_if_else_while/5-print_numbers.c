515bc1b82874f6c188ada1c62389ba691896c40d
Abdifatah <deenynur21@gmail.com>
2022-03-11 19:37:34 +0300

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
- * main - 5 print numbers
+/**
+ * main - Entry point 5 print numbers
+ *
  *
  * Return: Always 0 (Success)
  *
@@ -10,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		printf("%d", i);
 	}
