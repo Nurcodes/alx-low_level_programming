3c8ad8184aa952fa81bcaae26451d45499335f25
Abdifatah <deenynur21@gmail.com>
2022-03-11 19:25:12 +0300

    Task 4 alphabts

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Alphabet without q and e
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+		letter++;
+	}
+	putchar(10);
+	return (0);
+}
