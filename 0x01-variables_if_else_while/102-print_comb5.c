d4f924117df875b3acd8b014acb9e9463948db19
Abdifatah <deenynur21@gmail.com>
2022-03-14 15:06:54 +0300

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,23 +7,25 @@
  */
 int main(void)
 {
-	int a = 48;
-	int b = 49;
+	int a = 0;
+	int b = 0;
 
 	for (a = 0; a < 100; a++)
 	{
 		for (b = 1; b < 100; b++)
 		{
-			putchar((a / 10) + '0');
-			putchar((a % 10) + '0');
-			putchar(32);
-			putchar((b / 10) + '0');
-			putchar((b % 10) + '0');
-
-			if (a != 98 && b != 99)
+			if (a < b)
 			{
-				putchar(',');
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
 				putchar(32);
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (a != 98 && b != 99)
+				{
+					putchar(',');
+					putchar(32);
+				}
 			}
 		}
 	}
