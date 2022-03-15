dfe817525a72053bcda4c68abd998519623143d4
Abdifatah <deenynur21@gmail.com>
2022-03-15 16:12:27 +0300

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
 #include "main.h"
 void print_alphabet(void)
 {
-        int a = 'a';
-        while(a <= 'z')
-        {
-                _putchar(a);
-                a++;
-        }
-        _putchar(10);
+	int a = 'a';
+
+	while(a < 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar(10);
 }
