28f66425beff2f086ac559643946860acfc37760
Abdifatah <deenynur21@gmail.com>
2022-03-15 16:32:33 +0300

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10x
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	int a;
+
+	while (i <= 10)
+	{	a = 'a';
+
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar(10);
+		i++;
+	}
+}
