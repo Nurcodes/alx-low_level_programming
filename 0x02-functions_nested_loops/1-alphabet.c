5e9cc32a299b9a4e32de6dd8ac6375d03fa5ffef
Abdifatah <deenynur21@gmail.com>
2022-03-15 15:22:23 +0300

    Task 1 alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,3 +9,14 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
+
+void print_alphabet(void)
+{
+        int a = 'a';
+        while(a <= 'z')
+        {
+                _putchar(a);
+                a++;
+        }
+        _putchar(10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,4 @@
 int _putchar(char c);
-void print_alphabet(void)
-{
-	int a = 'a';
-	while(a <= 'z')
-	{
-		_putchar(a);
-		a++;
-	}
-	_putchar(10);
-}
+void print_alphabet(void);
 
 
