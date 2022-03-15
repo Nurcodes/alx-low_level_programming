57d28ca061992d1373cff0b382f9d6912998323a
Abdifatah <deenynur21@gmail.com>
2022-03-15 17:49:26 +0300

    Task 5 sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_sign - a function that prints the sign of a number
+ *@n: letter being tested
+ *Return: Always 0
+ */
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+		_putchar(43);
+		return (1);
+	} else if (n == 48)
+	{
+		_putchar(48);
+		return (0);
+	} else if
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
