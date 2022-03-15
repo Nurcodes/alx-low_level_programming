e764f632fcff51c7d84f6119c4185f94512da99b
Abdifatah <deenynur21@gmail.com>
2022-03-15 18:43:25 +0300

    Task 7 last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *print_last_digit - a function which returns the value of the last digit
+ *@a: letter being tested
+ * Return: Always 0
+ */
+int print_last_digit(int a)
+{
+	return (a % 10);
+}
