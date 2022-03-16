9b3be28e0dbe004117fb4d716164dee070a8bc2b
Abdifatah <deenynur21@gmail.com>
2022-03-16 20:52:26 +0300

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,7 @@ int main(void)
 	unsigned long int t1 = 0;
 	unsigned long int t2 = 1;
 	unsigned long int t3;
-	long long int i = 0;
+	unsigned long int i = 0;
 
 	while (i < 98)
 	{
@@ -21,4 +21,5 @@ int main(void)
 			printf("%ld, ", t3);
 		i++;
 	}
+	return (0);
 }
