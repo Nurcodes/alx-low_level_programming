fe0f3db5b3bd77a68d6a0b99bbb62cc51c0693f7
Abdifatah <deenynur21@gmail.com>
2022-03-16 17:31:01 +0300

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * get_mult - a function that gets the multiples of a number
- * @n: letter being tested
+ * multi - a function that get the multiples
+ * main - main entry point
  * Return: Always 0
  */
-int multi(int n)
+int main(void)
 {
-	int i;
-	int sum;
+	multi();
+	return (0);
+}
 
-	for (i = 0; i < n; i++)
+int multi(void)
+{
+	int i = 1;
+	int sum = 0;
+	int lim = 1024;
+	int b = 0;
+
+	for (i = b; i < lim; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i % 3 == 0 && i != 0)
 		{
 			sum += i;
+			printf("Multiple is %d and sum is: %d\n", i, sum);
+		}
+		else if (i % 5 == 0 && i != 0)
+		{	sum += i;
+			printf("Multiple is %d and sum is: %d\n", i, sum);
 		}
 	}
-	printf("%d", sum);
-	putchar('\n');
 	return (0);
 }
