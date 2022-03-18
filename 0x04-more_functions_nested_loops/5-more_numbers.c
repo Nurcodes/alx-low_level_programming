80ac973158cffa9f3f7d4c13a7faa06d4cae55ef
Abdifatah <deenynur21@gmail.com>
2022-03-18 21:44:29 +0300

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - a function which prints 10x 0-14
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 10; a++)
+	{
+		b = 0;
+
+		while (b <= 14)
+		{
+			if (b >= 10)
+			{
+				putchar(b / 10 + '0');
+			}
+			putchar(b % 10 + '0');
+
+			b++;
+		}
+		putchar(10);
+	}
+}
