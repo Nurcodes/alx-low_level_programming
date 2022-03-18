ce7dca8ed32bf9b99cdddc36d727d1fa280b4d90
Abdifatah <deenynur21@gmail.com>
2022-03-19 00:56:06 +0300

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	char Fizz[] = "Fizz";
+	char buzz[] = "Buzz";
+	char Fizzbuzz[] = "FizzBuzz";
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 5 == 0 && a % 3 == 0)
+			printf("%s ", Fizzbuzz);
+		else if (a % 3 == 0 && a % 5 != 0)
+			printf("%s ", Fizz);
+		else if (a % 5 == 0 && a % 3 != 0)
+			printf("%s ", buzz);
+		else
+			printf("%d ", a);
+		printf("\n");
+	}
+	printf("\n");
+	return (0);
+}
