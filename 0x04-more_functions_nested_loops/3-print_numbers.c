b1fd4d6aa94cbd4371b5d95b3ef3993132547d1b
Abdifatah <deenynur21@gmail.com>
2022-03-18 19:38:18 +0300

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - a functin which prints 0-9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int a = 48;
+	while (a > 58)
+	{
+		_putchar(a);
+		a++;
+	}
+}
