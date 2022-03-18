69a158486e47fba4ae333e8661d9a5ee29e3636e
Abdifatah <deenynur21@gmail.com>
2022-03-18 22:19:00 +0300

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - a function which prints a straight line
+ * @n: letter being tested
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar(10);
+	}
+	while (i < n)
+	{
+		_putchar(95);
+		i++;
+	}
+	putchar(10);
+}
