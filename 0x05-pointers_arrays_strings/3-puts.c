31be3c5ac815422db73b3382ff0b442bc7f6bf40
Abdifatah <deenynur21@gmail.com>
2022-03-21 19:47:47 +0300

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - a function which prints chars
+ * @str: letter being tested
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+}
