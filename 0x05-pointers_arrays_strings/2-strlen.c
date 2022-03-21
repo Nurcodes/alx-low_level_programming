4d9f41973a571784ab74ac2ee6d65b99c6740266
Abdifatah <deenynur21@gmail.com>
2022-03-21 19:26:19 +0300

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,15 @@
 #include "main.h"
-/*
- * _strlen - a function which gets the length of a string
- * @s: letter being tested
- * Return: Always 0
+/**
+ *_strlen - a function which gets the length of a string
+ *@s: letter being tested
+ *Return: Always 0
  */
 int _strlen(char *s)
 {
 	int n;
 	int len = 0;
 
-	for (n = 0; n < *s; n++)
+	for (n = 0; s[n] != '\0'; n++)
 	{
 		len++;
 	}
