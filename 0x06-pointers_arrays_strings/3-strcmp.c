859654d49d896aacd769601c9fc7f905b2bf3ee0
Abdifatah <deenynur21@gmail.com>
2022-03-24 23:49:26 +0300

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,34 +8,17 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int j = 0;
-	int s1_size;
-	int s2_size;
 	int score;
 
-	while (s1[i] != '\0')
+	while (s1[i] == s2[i] && (s1[i] != '\0' && s2[i] != '\0'))
 	{
 		i++;
 	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
 
-	s1_size = i;
-	s2_size = j;
-
-	if (s2_size > s1_size)
-	{
-		score = -1;
-	}
-	else if (s2_size < s1_size)
-	{
-		score = 1;
-	} else
-		score = 0;
+	score = s1[i] - s2[i];
 
 	return (score);
+
 
 
 }
