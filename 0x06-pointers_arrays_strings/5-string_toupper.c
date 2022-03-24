9d52e06d5ed4f3e7bbc8eb5a1e984c928c3d5d4c
Abdifatah <deenynur21@gmail.com>
2022-03-25 01:38:26 +0300

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
  * string_toupper - function
- * @*: letter
+ * @str: letter
  * Return: Always 0
  */
 char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'Z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
 		}
