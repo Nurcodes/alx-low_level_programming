40175e6b2f0312ef6363ee41f6396084e2ae454a
Abdifatah <deenynur21@gmail.com>
2022-03-25 00:40:32 +0300

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * reverse_array - reverse an array
+ * @a: array
+ * @n: letter being tested
+ * Return: Always 0
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int b[20];
+
+	for (i = 0; a[i] != a[n]; i++)
+	{
+		b[i] = a[i];
+	}
+	i = 0;
+
+	while (n > 0)
+	{
+		a[i] = b[n - 1];
+		i++;
+		n--;
+	}
+}
