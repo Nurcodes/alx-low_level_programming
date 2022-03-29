9c2f4a43a41de60fafeac8bbdde5e643b91690eb
Abdifatah <95445348+Nurcodes@users.noreply.github.com>
2022-03-29 19:36:20 +0000

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_memset - a function
+*@s: a letter
+*@b: a letter
+*@n: a letter
+*Return: Always 0
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+  int i;
+  for (i = 0; i < n; i++)
+  {
+    s[i] = b;
+  }
+  return (s);
+}
