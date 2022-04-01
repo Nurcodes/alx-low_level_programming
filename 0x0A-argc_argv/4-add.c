7f3fbf6a4c389a2f40eb0361e29165af3d44f6c4
Abdifatah <deenynur21@gmail.com>
2022-04-01 20:14:42 +0000

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - f
+ * @argc: 1
+ * @argv: 2
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	long int i;
+	long int sum = 0;
+	int flag;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] < 48 || *argv[i] > 57)
+		{
+			flag = 1;
+			break;
+		}
+		else if (*argv[i] >= 48 && *argv[i] <= 57)
+		{
+			sum += atoi(argv[i]);
+		}
+	}
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	if (flag == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+		printf("%li\n", sum);
+	return (0);
+}
