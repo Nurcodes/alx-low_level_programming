1f20179eecf421fc8de090034262f0b2d6838d2f
Abdifatah <deenynur21@gmail.com>
2022-04-01 17:31:20 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - function
+ * @argc: 1
+ * @argv: 2
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char **argv)
+{
+
+
+	if (argc == 1)
+	{
+		printf("%d\n", argc - 1);
+	}
+	else
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
