1d38ef4d76ba5c31ead7ac30625c51e881b4bc2c
Abdifatah <deenynur21@gmail.com>
2022-04-05 18:38:34 +0300

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,50 +1,42 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * str_concat - returns a pointer
- * @s1: first
- * @s2: second
- * Return: 0
+ * str_concat - function
+ * @s1: letter
+ * @s2: letter
+ * Return: ptr
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	int i, k, j, m;
 	char *ptr;
-	int n = 0;
+	int i, k, m, j;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
+	if (s1 == NULL)
 	{
-
+		s1 = "";
 	}
-	for (k = 0; s2[k] != '\0'; k++)
-	{
-
-	}
-	ptr = malloc(n + 2 * sizeof(char));
-	if (ptr == NULL)
-	{
-		printf("fail");
-		return (NULL);
-	}
-
+	if (s2 == NULL)
+		s2 = "";
 	for (j = 0; s1[j] != '\0'; j++)
 	{
-		ptr[j] = s1[j];
 	}
 	for (m = 0; s2[m] != '\0'; m++)
 	{
-		ptr[j] = s2[m];
-		j++;
 	}
-	ptr[j] = '\0';
-
+	ptr = malloc(1 + j + m * (sizeof(char)));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (k = 0; s2[k] != '\0'; k++)
+	{
+		ptr[i] = s2[k];
+	}
+		ptr[i] = '\0';
 
 	return (ptr);
-
-
-
 }
