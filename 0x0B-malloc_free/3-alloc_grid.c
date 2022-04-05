1a5b49a7600b9f120f24e89a3be5e163bf44dac5
Abdifatah <deenynur21@gmail.com>
2022-04-05 20:17:52 +0300

    Task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - a function
+ * @width: param
+ * @height: param
+ * Return: 0
+ */
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int j;
+	int **ptr;
+
+	if (width  <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	else if (width > 0 && height > 0)
+	{
+		ptr = (int **)malloc((height) * sizeof(int *));
+
+		for (i = 0; i < height; i++)
+		{
+			ptr[i] = malloc(width * sizeof(int));
+			for (j = 0; j < width; j++)
+			{
+				ptr[i][j] = 0;
+			}
+		}
+	}
+
+	return (ptr);
+
+
+
+}
