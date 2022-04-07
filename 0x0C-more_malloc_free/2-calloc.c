c264a2de7baf82175b533fbddc2f27e3abd9fc64
Abdifatah <deenynur21@gmail.com>
2022-04-08 00:18:39 +0300

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - function
+ * @nmemb: a
+ * @size: s
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+	int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(*ptr) * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	while (nmemb > 0)
+	{
+		ptr[i] = 0;
+		i++;
+		nmemb--;
+	}
+	return (ptr);
+
+
+}
