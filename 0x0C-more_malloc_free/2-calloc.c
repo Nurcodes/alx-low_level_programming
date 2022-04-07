c886f311ce1810e55b6cc8062c31f29215589187
Abdifatah <deenynur21@gmail.com>
2022-04-08 00:30:45 +0300

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
-	int i = 0;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -20,11 +20,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	while (nmemb)
+	for (i = 0; i < nmemb; i++)
 	{
 		ptr[i] = 0;
-		i++;
-		nmemb--;
 	}
 	return (ptr);
 
