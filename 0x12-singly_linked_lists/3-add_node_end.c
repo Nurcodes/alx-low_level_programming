254fcd992961b85eaf1e85b8f4e337c801a168b4
Abdifatah <deenynur21@gmail.com>
2022-04-21 22:46:57 +0300

    Task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node_end - adds newnode to end of list
+ *
+ * @head: double pointer to list head
+ *
+ * @str: string to be copied
+ *
+ * Return: new node at end
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *nwnode;
+	list_t *tail;
+	nwnode = malloc(sizeof(list_t));
+
+	if (nwnode == NULL)
+		return (NULL);
+	nwnode->len = _strlen(str);
+	nwnode->str = strdup(str);
+	if (nwnode->str == NULL)
+	{
+		free(nwnode);
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		*head = nwnode;
+		return (nwnode);
+	}
+	tail = *head;
+	while (tail->next != NULL)
+	{
+		tail = tail->next;
+	}
+	tail->next = nwnode;
+	return (nwnode);
+}
+
+/**
+ * _strlen - length
+ *
+ * @str: p
+ *
+ * Return: p
+ */
+
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
