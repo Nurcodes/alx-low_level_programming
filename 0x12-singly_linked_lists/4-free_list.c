358240fca521f4f730821e8cb3d0014c281cb9b3
Abdifatah <deenynur21@gmail.com>
2022-04-22 00:38:47 +0300

    Check

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -12,14 +12,12 @@
 
 void free_list(list_t *head)
 {
-	list_t *temp = head;
-	list_t *temp2 = temp;
-
+	list_t *temp = head;     
 	if (head != NULL)
 	{
 		while (temp->next != NULL)
 		{
-			temp2 = temp;
+			list_t *temp2 = temp;
 			temp = temp->next;
 			free(temp2);
 		}
