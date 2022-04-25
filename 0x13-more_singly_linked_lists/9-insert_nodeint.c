672c2fe3b658707d6583f12709237815f5f72f0b
Abdifatah <deenynur21@gmail.com>
2022-04-25 23:52:54 +0300

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		tmp = tmp->next;
 	}
-	if (tmp != NULL)
+	if (tmp->next != NULL && tmp != NULL)
 	{
 		new->next = tmp->next;
 		new->n = n;
