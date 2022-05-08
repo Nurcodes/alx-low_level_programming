1aea693b21f5333b04b7c3b085bb298b3d5e8123
Abdifatah <deenynur21@gmail.com>
2022-05-08 03:25:13 +0300

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,6 +13,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int i, y;
 	char *buf;
 
+	if (letters < 1)
+		return (0);
+
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
