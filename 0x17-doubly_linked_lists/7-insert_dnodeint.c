4b009a771511785a9ddba6e24586a8f055ecea6b
Abdifatah Nur <deenynur21@gmail.com>
2022-06-10 04:44:31 +0300

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * insert_dnodeint_at_index - inserts a new
- * @head: pointer to head of the list
+ * @h: pointer to head of the list
  * @idx: index to add at, starting from 0
  * @n: value of new node
  * Return: new node or null
