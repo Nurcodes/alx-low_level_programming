b3b3ace710a21b8ccdaaace0bfe5b7e7f3249329
Abdifatah Nur <deenynur21@gmail.com>
2022-07-05 16:03:46 +0300

    Task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Creates and returns a hashtable
+ *
+ * @size: unsigned long int size
+ * Return: Pointer to new hash table ro Null
+ *
+ */
+
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	if(!size)
+		return (NULL);
+
+	table = malloc(sizeof(hash_table_t*));
+
+	if(!table)
+		return (NULL);
+
+	table->array = malloc(size * sizeof(hash_node_t*));
+
+	if(!table->array)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	return(table);
+}
