a1e7799b075ed00bb9b4b0eca0cd892e0f7c2f6e
Abdifatah Nur <deenynur21@gmail.com>
2022-07-08 00:24:53 +0300

    Task 1

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - djb2 hash algorithm function
+ *
+ * @str: string to hash
+ *
+ * Return: A hased string
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+
+	return (hash);
+}
