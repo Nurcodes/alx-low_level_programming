70066271482491f3e102f358d291e5d44fe0595b
Abdifatah Nur <deenynur21@gmail.com>
2022-07-08 00:09:00 +0300

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "hash_tables.h"
 /**
  * hash_table_create - Creates and returns a hashtable
@@ -11,29 +12,24 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	unsigned long int i = 0;
+	unsigned int i = 0;
 
-	if (size <= 0)
-		return (NULL);
-
-	table = malloc(sizeof(hash_table_t) * 1);
+	table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
+	{
 		return (NULL);
+	}
 
+	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (table->array == NULL)
 	{
-		free(table->array);
-		free(table);
 		return (NULL);
 	}
-	while (i < size)
-	{
-		table->array[i] = NULL;
-		i++;
-	}
 
+	for (; i < size; i++)
+		table->array[i] = NULL;
 	return (table);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,8 +1,9 @@
+#ifndef HASH_TABLES
+#define HASH_TABLES
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#ifndef __HASH_TABLES_H
-#define __HASH_TABLES_H
+#include <string.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
